bf4e91ac66081bba69b24239bfed3595a6159f3e
Darius <dalvira@students.kennesaw.edu>
2013-07-24 17:06:42 -0400

    Added Cloud Backend
    username: dariusandleyth@gmail.com
    password: distributed13
    project name: CloudCourseProfiles
    project number: 434410060204
    project id: secret-meridian-286

--- CourseProfiles/gen/com/example/courseprofiles/R.java
+++ CourseProfiles/gen/com/example/courseprofiles/R.java
@@ -24,37 +24,40 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080018;
+        public static final int action_settings=0x7f08001a;
         public static final int addAssnBtn=0x7f080004;
-        public static final int addCourseBtn=0x7f080017;
+        public static final int addCourseBtn=0x7f080019;
         public static final int addExamBtm=0x7f080006;
         public static final int addProjectBtn=0x7f080007;
         public static final int addQuizBtn=0x7f080005;
-        public static final int confirmPasswordEdit=0x7f080012;
+        public static final int confirmPasswordEdit=0x7f080014;
         public static final int courseNameView=0x7f080001;
         public static final int emailEdit=0x7f080009;
-        public static final int firstNameEdit=0x7f08000e;
-        public static final int lastNameEdit=0x7f08000f;
-        public static final int majorView=0x7f080016;
+        public static final int firstNameEdit=0x7f080010;
+        public static final int lastNameEdit=0x7f080011;
+        public static final int majorView=0x7f080018;
+        public static final int msgView=0x7f08000e;
         public static final int passwordEdit=0x7f08000a;
         public static final int professorView=0x7f080002;
-        public static final int regEmailEdit=0x7f080010;
-        public static final int regPasswordEdit=0x7f080011;
-        public static final int registerBtn=0x7f080013;
+        public static final int regButton=0x7f08000f;
+        public static final int regEmailEdit=0x7f080012;
+        public static final int regPasswordEdit=0x7f080013;
+        public static final int registerBtn=0x7f080015;
         public static final int rememberBox=0x7f08000b;
-        public static final int schoolView=0x7f080015;
+        public static final int schoolView=0x7f080017;
         public static final int scrollView1=0x7f080000;
         public static final int semesterView=0x7f080003;
         public static final int signInBtn=0x7f08000c;
         public static final int signUpBtn=0x7f08000d;
-        public static final int studentNameView=0x7f080014;
+        public static final int studentNameView=0x7f080016;
         public static final int textView1=0x7f080008;
     }
     public static final class layout {
         public static final int activity_course_profile=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int activity_registration=0x7f030002;
-        public static final int activity_student_profile=0x7f030003;
+        public static final int activity_register=0x7f030002;
+        public static final int activity_registration=0x7f030003;
+        public static final int activity_student_profile=0x7f030004;
     }
     public static final class menu {
         public static final int course_profile=0x7f070000;
